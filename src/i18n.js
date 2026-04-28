e6f585e42b8e1e2f37cad436a0c3912d26af28d1
Renzo Bojórquez <u202411402@upc.edu.pe>
2026-04-28 17:34:13 -0500

    fix(i18n): fix filename route for import

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,6 @@
 import {createI18n} from "vue-i18n";
-import en from "./locales/en.js";
-import es from "./locales/es.js";
+import en from "./locales/en.json";
+import es from "./locales/es.json";
 const i18n = createI18n({
     legacy: false,
     locale: en,
